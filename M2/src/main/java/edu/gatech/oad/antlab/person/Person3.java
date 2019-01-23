3923f1c0129194f8e7b885d5748424550d17e419
Justin Deal <jdeal1912@gmail.com>
2019-01-23 15:49:16 -0500

    Edited Person 3 File

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Justin
  *  @version 1.1
  */
 public class Person3 {
@@ -42,7 +42,11 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	  char[] charArr = input.toCharArray();
+	  String myString = "";
+		for (int i = charArr.length; i >= 0; i--) {
+			myString += charArr;
+		}
+	  return myString;
 	}
 }
