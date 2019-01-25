9ee3b0de09cbbbc8db488a2c143d3b94d92e6a18
Jenny Guzdial <jguzdial6@gatech.edu>
2019-01-25 15:26:47 -0500

    Commiting Person5.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,7 +30,10 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
+        String word = input;
+        word = input.substring(2);
+        word = word + input.substring(0,2);
+        return word;
 	  return null;
 	}
 	
